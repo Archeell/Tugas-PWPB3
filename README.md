1b972a2cc4f064c1b0a38e5130ec1eb28f534073
Archeell <heiwa.alkahfi@gmail.com>
2022-08-06 10:28:27 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+## Setup
+- buka direktori project di gitBash
+- ketikan command : cp .env.example .env (untuk membuat file .env)
+- buat database dengan nama laravel
+
+Lalu ketik command dibawah ini
+- composer install
+- php artisan optimize:clear 
+- php artisan key:generate
+- php artisan migrate (migrasi database)
+- php artisan db:seed --class=UserSeeder (untuk mengisi data pada table users)
+
+## Login
+Untuk Login tambahkan '/login' pada penelusuran browser
+
+Email dan Password pada halaman Login
+Email : admin@gmail.com
+Password : password
+
+tambahan :
+- Setelah memasuki dashboard admin, silakah tambahkan 'admin/about' untuk ke halam crud about
+- Setelah memasuki dashboard admin, silakah tambahkan 'admin/portfolio' untuk ke halam crud portfolio
+- Setelah memasuki dashboard admin, silakah tambahkan 'admin/skill' untuk ke halam crud skill
---
+++ resources/views/layouts/frontend/app.blade.php
@@ -0,0 +1,204 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Portfolio | Muhammad Heiwa</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="{{ asset('templates/frontend/devfolio') }}/assets/img/favicon.png" rel="icon">
+  <link href="{{ asset('templates/frontend/devfolio') }}/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Vendor CSS Files -->
+  <link href="{{ asset('templates/frontend/devfolio') }}/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="{{ asset('templates/frontend/devfolio') }}/assets/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  <link href="{{ asset('templates/frontend/devfolio') }}/assets/vendor/ionicons/css/ionicons.min.css" rel="stylesheet">
+  <link href="{{ asset('templates/frontend/devfolio') }}/assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="{{ asset('templates/frontend/devfolio') }}/assets/vendor/venobox/venobox.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="{{ asset('templates/frontend/devfolio') }}/assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: DevFolio - v2.4.0
+  * Template URL: https://bootstrapmade.com/devfolio-bootstrap-portfolio-html-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body id="page-top">
+
+  <!-- ======= Header/ Navbar ======= -->
+  <nav class="navbar navbar-b navbar-trans navbar-expand-md fixed-top" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand js-scroll" href="#page-top">DASHBOARD</a>
+      <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarDefault" aria-controls="navbarDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+      <div class="navbar-collapse collapse justify-content-end" id="navbarDefault">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link js-scroll active" href="#home">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll" href="#about">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll" href="#contact">Contact</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll" href="javascript:void(0)" onclick="window.open('/login')">Login</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!-- ======= Intro Section ======= -->
+  <div id="home" class="intro route bg-dark">
+    <div class="overlay-itro"></div>
+    <div class="intro-content display-table">
+      <div class="table-cell">
+        <div class="container">
+          <!--<p class="display-6 color-d">Hello, world!</p>-->
+          <h1 class="intro-title mb-4">Muhammad Heiwa</h1>
+          <p class="intro-subtitle"><span class="text-slider-items">Student of SMKN 1 Subang</span><strong class="text-slider"></strong></p>
+          <!-- <p class="pt-3"><a class="btn btn-primary btn js-scroll px-4" href="#about" role="button">Learn More</a></p> -->
+        </div>
+      </div>
+    </div>
+  </div><!-- End Intro Section -->
+
+  <main id="main">
+
+    <!-- ======= About Section ======= -->
+    <section id="about" class="about-mf sect-pt4 route">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12">
+            <div class="box-shadow-full">
+              <div class="row">
+                <div class="col-md-6">
+                  <div class="row">
+                    <div class="col-sm-6 col-md-5">
+                      <div class="about-img">
+                        <img style="height: 200px; object-fit: cover; object-position: center;" class="img-fluid rounded b-shadow-a" src="img/foto.jpg" style="background-image: url('storage/app/public/uploads/about/foto.jpg')" alt="">
+                      </div>
+                    </div>
+                    <div class="col-sm-6 col-md-7">
+                      <div class="about-info">
+                        <p><span class="title-s">Name: </span> <span>Muhammad Heiwa Anujesi Alkahfi</span></p>
+                        <p><span class="title-s">Role: </span> <span>tudent</span></p>
+                        <p><span class="title-s">Email: </span> <span>heiwaalkahfi@gmail.com</span></p>
+                        <p><span class="title-s">Phone: </span> <span>082353194501</span></p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-6">
+                  <div class="about-me pt-4 pt-md-0">
+                    <div class="title-box-2">
+                      <h5 class="title-left">
+                        About Me
+                      </h5>
+                    </div>
+                    <p class="lead">
+                      #1 Champion in Class
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section><!-- End About Section -->
+
+    <!-- ======= Contact Section ======= -->
+    <section class="paralax-mf footer-paralax bg-image sect-mt4 route">
+      <div class="overlay-mf"></div>
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12">
+            <div class="contact-mf">
+              <div id="contact" class="box-shadow-full">
+                <div class="row">
+                  <div class="col-md-6 mx-auto">
+                    <div class="title-box-2 pt-4 pt-md-0">
+                      <h5 class="title-left">
+                        Contact Me
+                      </h5>
+                    </div>
+                    <div class="more-info">
+                      <p class="lead">
+                      Follow my social media
+                      </p>
+                    </div>
+                    <div class="socials">
+                      <ul>
+                        <li><a href="javascript:void(0)" onclick="window.open('https://www.facebook.com/Muhammad.heiwa/')"><span class="ico-circle"><i class="ion-social-facebook"></i></span></a></li>
+                        <li><a href="javascript:void(0)" onclick="window.open('https://https://www.instagram.com/m.heiwa_a/?hl=id/')"><span class="ico-circle"><i class="ion-social-instagram"></i></span></a></li>
+                        <li><a href="javascript:void(0)" onclick="window.open('https://https://www.youtube.com/channel/UCb6d9-T2i40vboaCp5glGsw/')"><span class="ico-circle"><i class="ion-social-youtube"></i></span></a></li>
+                        <li><a href="javascript:void(0)" onclick="window.open('https://https://github.com/Archeell/')"><span class="ico-circle"><i class="ion-social-github"></i></span></a></li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section><!-- End Contact Section -->
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12">
+          <div class="copyright-box">
+            <p class="copyright">&copy; Copyright <strong>{{ $getFooter->copyright ?? 'Alkahfi Copyright'  }}</strong>. All Rights Reserved</p>
+            <div class="credits">
+              <!--
+              All the links in the footer should remain intact.
+              You can delete the links only if you purchased the pro version.
+              Licensing information: https://bootstrapmade.com/license/
+              Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=DevFolio
+            -->
+              Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer><!-- End  Footer -->
+
+  <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+  <div id="preloader"></div>
+
+  <!-- Vendor JS Files -->
+  <script src="{{ asset('templates/frontend/devfolio') }}/assets/vendor/jquery/jquery.min.js"></script>
+  <script src="{{ asset('templates/frontend/devfolio') }}/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="{{ asset('templates/frontend/devfolio') }}/assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+  <script src="{{ asset('templates/frontend/devfolio') }}/assets/vendor/php-email-form/validate.js"></script>
+  <script src="{{ asset('templates/frontend/devfolio') }}/assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+  <script src="{{ asset('templates/frontend/devfolio') }}/assets/vendor/counterup/jquery.counterup.min.js"></script>
+  <script src="{{ asset('templates/frontend/devfolio') }}/assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+  <script src="{{ asset('templates/frontend/devfolio') }}/assets/vendor/typed.js/typed.min.js"></script>
+  <script src="{{ asset('templates/frontend/devfolio') }}/assets/vendor/venobox/venobox.min.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="{{ asset('templates/frontend/devfolio') }}/assets/js/main.js"></script>
+
+</body>
+
+</html>
